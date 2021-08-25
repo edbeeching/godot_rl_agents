60dd796c40f005211d3d466ca9a2ff12b2b501dc
Edward Beeching <edbeeching@gmail.com>
2021-08-25 14:20:46 +0200

    Added a quick test for the godot env with random actions and resets

---
+++ godot_rl_agents/tests/test_godot_env.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from godot_rl_agents.core.godot_env import GodotEnv
+
+
+if __name__ == "__main__":
+
+    env = GodotEnv(env_path=None, port=10008)
+
+    print("RESET ENV 1")
+    obs = env.reset()
+    for i in range(10):
+        print("sending action")
+        _ = env.step(np.random.uniform(-1.0, 1.0, size=(env.num_envs, 2)))
+    print("RESET ENV 2")
+    obs = env.reset()
+    obs = env.reset()
+    print("RESET ENV 2 DONE")
+    for i in range(10):
+        print("sending action")
+        _ = env.step(np.random.uniform(-1.0, 1.0, size=(env.num_envs, 2)))
+    print("RESET ENV 2")
+    obs = env.reset()
+    print("RESET ENV 2 DONE")
+    for i in range(10):
+        print("sending action")
+        _ = env.step(np.random.uniform(-1.0, 1.0, size=(env.num_envs, 2)))
+
+    env.close()
