106ae8f0bdf9541b6d394e757e2af805eff98601
Edward Beeching <edbeeching@gmail.com>
2021-08-30 14:37:20 +0200

    Created the jumper environment

---
+++ envs/Jumper/RaycastSensor.gd
@@ -0,0 +1,25 @@
+extends Spatial
+
+
+var raycasts = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    raycasts = get_children()
+
+func _physics_process(delta):
+    get_raycast_buffer()
+    
+func get_raycast_buffer():
+    var raycast_buffer := []
+    for child in raycasts:
+        raycast_buffer.append(_get_raycast_distance(child))
+    return raycast_buffer
+    
+func _get_raycast_distance(rc : RayCast) -> float : 
+    if !rc.is_colliding():
+        return 0.0
+         
+    var distance = (get_translation() - rc.get_collision_point()).length()
+    distance = clamp(distance, 0.0, 200.0)
+    return (200.0 - distance) / 200.0
