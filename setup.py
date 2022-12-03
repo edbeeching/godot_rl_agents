7fc50433a5c98e9a29adea987f54cd510d807e03
Edward Beeching <edbeeching@gmail.com>
2022-12-03 13:41:19 +0100

    updated setup file and removed old CI files

--- setup.py
+++ setup.py
@@ -1,36 +1,60 @@
-# import setuptools
+from distutils.core import setup
+from setuptools import find_packages
 
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
+__version__ = "0.3.0"  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
 
-# setuptools.setup(
-#     name="godot_rl_agents",
-#     version="0.0.1",
-#     author="Edward Beeching",
-#     author_email="edbeeching@gmail.com",
-#     description="A Deep Reinforcement Learning package for the Godot game engine",
-#     long_description=long_description,
-#     long_description_content_type="text/markdown",
-#     url="https://github.com/pypa/edbeeching/godot_rl_agents",
-#     project_urls={
-#         "Bug Tracker": "https://github.com/pypa/edbeeching/godot_rl_agents/issues",
-#     },
-#     classifiers=[
-#         "Programming Language :: Python :: 3",
-#         "License :: OSI Approved :: MIT License",
-#         "Operating System :: OS Independent",
-#     ],
-#     # packages=setuptools.find_packages(where="godot_rl_agents"),
-#     packages=[
-#         "godot_rl_agents",
-#         "godot_rl_agents.core",
-#         "godot_rl_agents.tests",
-#     ],
-#     python_requires=">=3.6",
-# )
+REQUIRED_PKGS = [
+    "numpy",
+    "torch",
+    "huggingface_hub>=0.10",  # For sharing objects, environments & trained RL policies
+    "gym==0.21",  # For RL action spaces and API
+    "stable-baselines3"
+]
 
 
-from setuptools import setup
+DEV_REQUIRE = []
 
-if __name__ == "__main__":
-    setup(include_package_data=True)
+TESTS_REQUIRE = [
+    "pytest",
+    "pytest-xdist",
+]
+SB3_REQUIRE = ["stable-baselines3"]
+RLLIB_REQUIRE = ["ray[rllib]"]
+SAMPLE_FACTORY_REQUIRE = ["sample-factory"]
+QUALITY_REQUIRE = ["black[jupyter]~=22.0", "flake8>=3.8.3", "isort>=5.0.0", "pyyaml>=5.3.1"]
+
+EXTRAS_REQUIRE = {
+    "dev": DEV_REQUIRE + TESTS_REQUIRE + QUALITY_REQUIRE,
+    "test": TESTS_REQUIRE,
+}
+
+
+setup(
+    name="godot_rl_agents",
+    version=__version__,
+    description="A Deep Reinforcement Learning package for the Godot game engine",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Edward Beeching.",
+    author_email="edbeeching@gmail.com",
+    url="https://github.com/edbeeching/godot_rl_agents",
+    download_url="hhttps://github.com/edbeeching/godot_rl_agents/tags",
+    license="MIT",
+    package_dir={"": "./"},
+    packages=find_packages(where="./", include="godot_rl_agents*"),
+    install_requires=REQUIRED_PKGS,
+    extras_require=EXTRAS_REQUIRE,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    keywords="simulation environments machine learning reinforcement learning deep learning video games godot",
+    zip_safe=False,  # Required for mypy to find the py.typed file
+    python_requires=">=3.8",
+)
