a289564034c594e20bcdfe73284923b738c36e27
Edward Beeching <edbeeching@gmail.com>
2023-01-16 20:37:56 +0100

    Adapts CleanRL wrapper to work with all Tuples of Box action spaces

--- godot_rl/wrappers/clean_rl_wrapper.py
+++ godot_rl/wrappers/clean_rl_wrapper.py
@@ -8,41 +8,57 @@ from godot_rl.core.godot_env import GodotEnv
 class CleanRLGodotEnv:
     def __init__(self, env_path=None, group_tuple_actions=False, **kwargs):
         # group_tuple_actions: combine multiple continue action spaces into one larger space 
-        self.env = GodotEnv(env_path=env_path, **kwargs)
+        self._env = GodotEnv(env_path=env_path, **kwargs)
         self._check_valid_action_space(group_tuple_actions)
+        self._group_tuple_actions = group_tuple_actions
+        
 
     def _check_valid_action_space(self, group_tuple_actions):
-        action_space = self.env.action_space
+        action_space = self._env.action_space
         if isinstance(action_space, gym.spaces.Tuple):
             if group_tuple_actions:
+                sizes = [s.shape[0] for s in action_space]
+                def preprocessor(action):
+                    return np.expand_dims(action, axis=2)
+                self.action_preprocessor = preprocessor
+
+                
+                self._flattened_action_space = gym.spaces.Box(action_space[0].low[0],
+                                                                action_space[0].high[0], 
+                                                                shape=[sum(sizes)])
 
             else:
                 assert (
                     len(action_space.spaces) == 1
                 ), f"clearn rl supports a single action space, this env constains multiple spaces {action_space}"
 
-    def action_preprocessor(self, action):
-        return action
+    @staticmethod
+    def action_preprocessor(action):
+        return [action]
 
     def step(self, action):
         action = self.action_preprocessor(action)
-        obs, reward, term, trunc, info = self.env.step([action])
+        obs, reward, term, trunc, info = self._env.step(action, order_ij=True)
         obs = lod_to_dol(obs)
         return np.stack(obs["obs"]), reward, term, trunc, info
 
     def reset(self, seed):
-        obs, info = self.env.reset(seed)
+        obs, info = self._env.reset(seed)
         obs = lod_to_dol(obs)
         return np.stack(obs["obs"]), info
 
     @property
     def single_observation_space(self):
-        return self.env.observation_space["obs"]
+        return self._env.observation_space["obs"]
 
     @property
     def single_action_space(self):
-        return self.env.action_space[0]
+        if not self._group_tuple_actions:
+            return self._env.action_space[0]
+        
+        # flatten the action space
+        return self._flattened_action_space
 
     @property
     def num_envs(self):
-        return self.env.num_envs
+        return self._env.num_envs
