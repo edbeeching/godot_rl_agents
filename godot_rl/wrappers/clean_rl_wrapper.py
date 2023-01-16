2c751414620ebcd067f06e7c2d4622d88e66b121
Edward Beeching <edbeeching@gmail.com>
2023-01-16 12:30:48 +0100

    adding tuple dists

--- godot_rl/wrappers/clean_rl_wrapper.py
+++ godot_rl/wrappers/clean_rl_wrapper.py
@@ -6,18 +6,26 @@ from godot_rl.core.godot_env import GodotEnv
 
 
 class CleanRLGodotEnv:
-    def __init__(self, env_path=None, **kwargs):
+    def __init__(self, env_path=None, group_tuple_actions=False, **kwargs):
+        # group_tuple_actions: combine multiple continue action spaces into one larger space 
         self.env = GodotEnv(env_path=env_path, **kwargs)
-        self._check_valid_action_space()
+        self._check_valid_action_space(group_tuple_actions)
 
-    def _check_valid_action_space(self):
+    def _check_valid_action_space(self, group_tuple_actions):
         action_space = self.env.action_space
         if isinstance(action_space, gym.spaces.Tuple):
-            assert (
-                len(action_space.spaces) == 1
-            ), f"clearn rl supports a single action space, this env constains multiple spaces {action_space}"
+            if group_tuple_actions:
+
+            else:
+                assert (
+                    len(action_space.spaces) == 1
+                ), f"clearn rl supports a single action space, this env constains multiple spaces {action_space}"
+
+    def action_preprocessor(self, action):
+        return action
 
     def step(self, action):
+        action = self.action_preprocessor(action)
         obs, reward, term, trunc, info = self.env.step([action])
         obs = lod_to_dol(obs)
         return np.stack(obs["obs"]), reward, term, trunc, info
