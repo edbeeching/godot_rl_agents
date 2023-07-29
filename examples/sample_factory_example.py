75760eaa0e08a097447fc638bf1e6d3b8b8e001a
Ivan-267 <61947090+Ivan-267@users.noreply.github.com>
2023-07-29 22:02:01 +0200

    Revert "Another attempt to add seed"
    This reverts commit 6949d4abac5e61770f59762f759dcb874aa4a336.

--- examples/sample_factory_example.py
+++ examples/sample_factory_example.py
@@ -7,8 +7,7 @@ def get_args():
     parser.add_argument("--env_path", default=None, type=str, help="Godot binary to use")
     parser.add_argument("--eval", default=False, action="store_true", help="whether to eval the model")
     parser.add_argument("--speedup", default=1, type=int, help="whether to speed up the physics in the env")
-    parser.add_argument("--seed", default=0, type=int, help="environment seed")
-    parser.add_argument("--export", default=False, action="store_true", help="whether to export the model")
+    parser.add_argument("--export", default=False, action="store_true", help="wheter to export the model")
     parser.add_argument("--viz", default=False, action="store_true", help="Whether to visualize one process")
 
     return parser.parse_known_args()
