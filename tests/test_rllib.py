497abd3078cf72ace75b1deacd86a9fe5e5ac73c
Ivan-267 <61947090+Ivan-267@users.noreply.github.com>
2023-09-23 14:09:44 +0200

    Restore the test config path changed for testing

--- tests/test_rllib.py
+++ tests/test_rllib.py
@@ -9,7 +9,7 @@ def test_rllib_training():
     from godot_rl.wrappers.ray_wrapper import rllib_training
     from godot_rl.main import get_args
     args, extras = get_args()
-    args.config_file = "fixtures/test_rllib.yaml"
+    args.config_file = "tests/fixtures/test_rllib.yaml"
     args.env_path = "examples/godot_rl_JumperHard/bin/JumperHard.x86_64"
 
     
