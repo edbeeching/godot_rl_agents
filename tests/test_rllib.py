ddf7c94011e1c4d7295650fabb6937a7e2ae30fe
Ivan-267 <61947090+Ivan-267@users.noreply.github.com>
2023-09-23 14:02:07 +0200

    Convert paths to absolute for rllib

--- tests/test_rllib.py
+++ tests/test_rllib.py
@@ -1,3 +1,5 @@
+import os.path
+
 import pytest
 
 from godot_rl.core.utils import cant_import
@@ -7,7 +9,7 @@ def test_rllib_training():
     from godot_rl.wrappers.ray_wrapper import rllib_training
     from godot_rl.main import get_args
     args, extras = get_args()
-    args.config_file = "tests/fixtures/test_rllib.yaml"
+    args.config_file = "fixtures/test_rllib.yaml"
     args.env_path = "examples/godot_rl_JumperHard/bin/JumperHard.x86_64"
 
     
