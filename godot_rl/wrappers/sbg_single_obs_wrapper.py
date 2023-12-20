14c45b898b4b3aa2995c4d3b53df9e76716d42b4
Ivan-267 <61947090+Ivan-267@users.noreply.github.com>
2023-12-20 15:30:49 +0100

    Remove unused imports, reformat wrapper

--- godot_rl/wrappers/sbg_single_obs_wrapper.py
+++ godot_rl/wrappers/sbg_single_obs_wrapper.py
@@ -1,13 +1,9 @@
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Tuple
 
 import gymnasium as gym
 import numpy as np
-from godot_rl.core.godot_env import GodotEnv
-from godot_rl.core.utils import can_import, lod_to_dol
-from stable_baselines3 import PPO
 from godot_rl.wrappers.stable_baselines_wrapper import StableBaselinesGodotEnv
-from stable_baselines3.common.vec_env.base_vec_env import VecEnv
-from stable_baselines3.common.vec_env.vec_monitor import VecMonitor
+
 
 # A variant of the Stable Baselines Godot Env that only supports a single obs space from the dictionary - obs["obs"].
 # This provides some basic support for using envs that have a single obs space with policies other than MultiInputPolicy.
@@ -17,7 +13,7 @@ class SBGSingleObsEnv(StableBaselinesGodotEnv):
         super().__init__(*args, **kwargs)
 
     def step(self, action: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray, List[Dict[str, Any]]]:
-        obs, rewards, term, info  = super().step(action)
+        obs, rewards, term, info = super().step(action)
         return obs["obs"], rewards, term, info
 
     def reset(self) -> np.ndarray:
@@ -26,4 +22,4 @@ class SBGSingleObsEnv(StableBaselinesGodotEnv):
 
     @property
     def observation_space(self) -> gym.Space:
-            return self.envs[0].observation_space["obs"]
+        return self.envs[0].observation_space["obs"]
