f44f851a5f68b79a5bb165bec85b19b11a94e2a3
Ivan-267 <61947090+Ivan-267@users.noreply.github.com>
2023-12-22 14:28:30 +0100

    Add dictionary key setting to sbg_single_obs_wrapper.py

--- godot_rl/wrappers/sbg_single_obs_wrapper.py
+++ godot_rl/wrappers/sbg_single_obs_wrapper.py
@@ -5,21 +5,22 @@ import numpy as np
 from godot_rl.wrappers.stable_baselines_wrapper import StableBaselinesGodotEnv
 
 
-# A variant of the Stable Baselines Godot Env that only supports a single obs space from the dictionary - obs["obs"].
+# A variant of the Stable Baselines Godot Env that only supports a single obs space from the dictionary - obs["obs"] by default.
 # This provides some basic support for using envs that have a single obs space with policies other than MultiInputPolicy.
 
 class SBGSingleObsEnv(StableBaselinesGodotEnv):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, obs_key="obs", *args, **kwargs) -> None:
+        self.obs_key = obs_key
         super().__init__(*args, **kwargs)
 
     def step(self, action: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray, List[Dict[str, Any]]]:
         obs, rewards, term, info = super().step(action)
-        return obs["obs"], rewards, term, info
+        return obs[self.obs_key], rewards, term, info
 
     def reset(self) -> np.ndarray:
         obs = super().reset()
-        return obs["obs"]
+        return obs[self.obs_key]
 
     @property
     def observation_space(self) -> gym.Space:
-        return self.envs[0].observation_space["obs"]
+        return self.envs[0].observation_space[self.obs_key]
