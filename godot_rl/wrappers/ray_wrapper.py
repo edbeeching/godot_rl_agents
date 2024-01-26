50eb247485653c1b328f4a1436d34cbe80b532f8
Ivan-267 <61947090+Ivan-267@users.noreply.github.com>
2024-01-26 16:47:23 +0100

    Reformat
    Reformat using black-24.1.0

--- godot_rl/wrappers/ray_wrapper.py
+++ godot_rl/wrappers/ray_wrapper.py
@@ -181,9 +181,9 @@ def rllib_training(args, extras):
             checkpoint_at_end=not args.eval,
             restore=args.restore,
             local_dir=os.path.abspath(args.experiment_dir) or os.path.abspath("logs/rllib"),
-            trial_name_creator=lambda trial: f"{args.experiment_name}"
-            if args.experiment_name
-            else f"{trial.trainable_name}_{trial.trial_id}",
+            trial_name_creator=lambda trial: (
+                f"{args.experiment_name}" if args.experiment_name else f"{trial.trainable_name}_{trial.trial_id}"
+            ),
         )
     if args.export:
         raise NotImplementedError("Exporting is not (re)implemented yet")
