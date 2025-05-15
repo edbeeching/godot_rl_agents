c1f9f799aa728577787a2af139a2787842bbdb68
Ivan-267 <61947090+Ivan-267@users.noreply.github.com>
2025-05-15 13:15:43 +0200

    Update clean_rl_pqn_example.py
    Fixes the comment

--- examples/clean_rl_pqn_example.py
+++ examples/clean_rl_pqn_example.py
@@ -57,9 +57,9 @@ class Args:
     total_timesteps: int = 1_000_000
     """total timesteps of the experiments"""
     learning_rate: float = 2.5e-4
-    """the learning rate of the optimizer [note: automatically set]"""
+    """the learning rate of the optimizer"""
     num_envs: int = 4
-    """the number of parallel game environments"""
+    """the number of parallel game environments [note: automatically set]"""
     num_steps: int = 128
     """the number of steps to run for each environment per update"""
     num_minibatches: int = 4
